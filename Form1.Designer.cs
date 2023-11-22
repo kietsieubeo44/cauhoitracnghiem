ce4c54eec23b722f113fa91cc238754e854af8b7
kiet <kietsieubeo44@gmail.com>
2023-11-22 13:59:42 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace cauhoitracnghiem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCauHoi = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.rbB = new System.Windows.Forms.RadioButton();
+            this.rbC = new System.Windows.Forms.RadioButton();
+            this.rbD = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lblCauHoi
+            // 
+            this.lblCauHoi.AutoSize = true;
+            this.lblCauHoi.Location = new System.Drawing.Point(55, 72);
+            this.lblCauHoi.Name = "lblCauHoi";
+            this.lblCauHoi.Size = new System.Drawing.Size(52, 16);
+            this.lblCauHoi.TabIndex = 1;
+            this.lblCauHoi.Text = "Câu hỏi";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(445, 259);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Location = new System.Drawing.Point(58, 161);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(37, 20);
+            this.rbA.TabIndex = 3;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "A";
+            this.rbA.UseVisualStyleBackColor = true;
+            // 
+            // rbB
+            // 
+            this.rbB.AutoSize = true;
+            this.rbB.Location = new System.Drawing.Point(185, 161);
+            this.rbB.Name = "rbB";
+            this.rbB.Size = new System.Drawing.Size(37, 20);
+            this.rbB.TabIndex = 4;
+            this.rbB.TabStop = true;
+            this.rbB.Text = "B";
+            this.rbB.UseVisualStyleBackColor = true;
+            // 
+            // rbC
+            // 
+            this.rbC.AutoSize = true;
+            this.rbC.Location = new System.Drawing.Point(324, 161);
+            this.rbC.Name = "rbC";
+            this.rbC.Size = new System.Drawing.Size(37, 20);
+            this.rbC.TabIndex = 5;
+            this.rbC.TabStop = true;
+            this.rbC.Text = "C";
+            this.rbC.UseVisualStyleBackColor = true;
+            // 
+            // rbD
+            // 
+            this.rbD.AutoSize = true;
+            this.rbD.Location = new System.Drawing.Point(482, 161);
+            this.rbD.Name = "rbD";
+            this.rbD.Size = new System.Drawing.Size(38, 20);
+            this.rbD.TabIndex = 6;
+            this.rbD.TabStop = true;
+            this.rbD.Text = "D";
+            this.rbD.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 336);
+            this.Controls.Add(this.rbD);
+            this.Controls.Add(this.rbC);
+            this.Controls.Add(this.rbB);
+            this.Controls.Add(this.rbA);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblCauHoi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCauHoi;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.RadioButton rbA;
+        private System.Windows.Forms.RadioButton rbB;
+        private System.Windows.Forms.RadioButton rbC;
+        private System.Windows.Forms.RadioButton rbD;
+    }
+}
+
